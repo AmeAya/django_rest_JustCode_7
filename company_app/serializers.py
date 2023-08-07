f61d64e8b3e4e6da0725c778f03046e2e52db666
ameaya <deobackstep@gmail.com>
2023-08-07 20:29:16 +0600

    Serializer custom data

--- company_app/serializers.py
+++ company_app/serializers.py
@@ -7,3 +7,29 @@ class CompanySerializer(ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+    # to_representation - Это и есть обработчик в JSON
+    # instance - Объект модели, которая приходит в сериалайзер
+    # Функция должна вернуть словарь
+    def to_representation(self, instance):
+        representation = dict()
+        representation['id'] = instance.id
+        representation['name'] = instance.name
+        representation['bin'] = int(instance.bin)
+        representation['country'] = instance.country
+        representation['employee_count'] = instance.employee_count
+        representation['industry'] = instance.industry.name
+        if instance.parent:
+            representation['parent'] = instance.parent.name
+        return representation
+
+    # Второй вариант. Использование родительского to_representation()
+
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     representation['industry'] = instance.industry.name
+    #     if representation['parent']:
+    #         representation['parent'] = instance.parent.name
+    #     else:
+    #         del representation['parent']
+    #     return representation
